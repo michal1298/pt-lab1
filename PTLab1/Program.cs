d642642c4faa7a21208cdf50acad431bf024bc22
michal1298 <michal.jank98@gmail.com>
2021-03-20 11:36:12 +0100

    Add display folder names

--- PTLab1/Program.cs
+++ PTLab1/Program.cs
@@ -7,14 +7,36 @@ namespace PTLab1
     {
         static void Task1(string path)
         {
-            Console.WriteLine("Task1 function");
-            Console.WriteLine(Directory.GetFiles(path));
+            // znaleźć wszystkie katalogi w folderze głównym
+            // wejść do katalogu
+            // wyświetlić pliki z tego katalogu
+            // wyświetlić nazwę katalogu
+            // wejść do katalogu
+            // wyświetlić pliki z katalogu
+            
+            //Console.WriteLine("Task1 function");
+            var allDirectories = Directory.GetDirectories(path);
+
+            Console.WriteLine("==============================");
+            Console.WriteLine("number of folders in give path: " + allDirectories.Length);
+
+            Console.WriteLine("all directories:");
+            foreach (var i in allDirectories)       // wyświetlenie katalogów
+            {
+                //Console.WriteLine("directory foreach: " + i);
+                Console.WriteLine(Path.GetFileName(i));         // to jest dobrze
+                //Console.WriteLine(i);
+            }
+            Console.WriteLine("==============================");
+
+            //Console.WriteLine("allDirectories: " + allDirectories); // źle wyświetla
+            //Console.WriteLine(Directory.GetFiles(path));    // źle działa
             string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
             Console.WriteLine("The number of files in given directory is {0}. All files:", files.Length);
 
             for (int i = 0; i < files.Length; i++)
             {
-                Console.WriteLine(files[i]);
+                //Console.WriteLine(files[i]);
                 Console.WriteLine("Without path: " + Path.GetFileName(files[i]));
                 //Console.WriteLine(files.DirectorySeparatorChar);
                 //Path.DirectorySeparatorChar(files[i]);
