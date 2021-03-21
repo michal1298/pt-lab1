c1d77f256f473123f174239f914f749a2a4b7022
michal1298 <michal.jank98@gmail.com>
2021-03-21 11:29:08 +0100

    Add display number of subdirectories and files in directory

--- PTLab1/Program.cs
+++ PTLab1/Program.cs
@@ -1,5 +1,43 @@
 ﻿using System;
 using System.IO;    // do Directory.GetFiles
+using System.Linq;  // do Count()
+using System.Collections.Generic;
+
+
+// metody rozszerzające (extension methods):
+namespace PTLab1
+{
+    // rozszerzyć System.IO.FileSystemInfo o zwrócenie napisu reprezentującego
+    // atrybuty dostępowe danego pliku/katalogu w postaci sformatowanej:
+    public static class ExtensionFileSytemInfo
+    {
+        public static void PrintAttributes(this FileSystemInfo path)
+        {
+            Console.WriteLine("PrintAttributes function");
+        }
+    }
+
+
+    // rozszerzyć System.IO.DirectoryInfo o liczbę elementów w katalogu:
+    public static class ExtensionDirectoryInfo
+    {
+        public static int NumberOfDirectories(this DirectoryInfo myDirectory)
+        {
+            var numberOfDirectories = myDirectory.GetDirectories().Count();
+            //var numberOfFiles = myDirectory.GetFiles().Count();
+
+            return numberOfDirectories;
+        }
+        
+        public static int NumberOfFiles(this DirectoryInfo myDirectory)
+        {
+            var numberOfFiles = myDirectory.GetFiles().Count();
+            return numberOfFiles;
+        }   
+        
+    }
+}
+
 
 namespace PTLab1
 {
@@ -13,40 +51,6 @@ namespace PTLab1
             //Console.WriteLine("  number of FILES in given path is {0}.", allFiles.Length);
             Files(path, countBackslashStartPath);
             Catalogs(path, countBackslashStartPath);
-
-
-            /*
-            foreach (var subdirectory in allDirectories)       // wyświetlenie katalogów
-            {
-                Console.WriteLine("==============================");
-                Console.WriteLine("number of FOLDERS in given path is {0}. All folders:", allDirectories.Length);
-
-
-
-
-                Console.WriteLine(Path.GetFileName(subdirectory));         // to jest dobrze
-                //var allDirectoriesInFolder = Directory.GetDirectories(subdirectory);
-                Catalogs(subdirectory);     // rekurencja
-            }
-
-            Console.WriteLine("==============================");
-
-            
-            
-            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);      // todo tutaj zmienić
-            Console.WriteLine("number of FILES in given folder is {0}. All files:", files.Length);
-            
-            for (int i = 0; i < files.Length; i++)
-            {
-                //Console.WriteLine(files[i]);
-                Console.WriteLine("\t" + Path.GetFileName(files[i]));
-                //Console.WriteLine(files.DirectorySeparatorChar);
-                //Path.DirectorySeparatorChar(files[i]);
-                //Console.WriteLine("type of files[i]: " + files[i].GetType());       // System.String
-
-            }
-           
-            */
         }
 
         static void Files(string path, int countBackslashStartPath)
@@ -67,8 +71,13 @@ namespace PTLab1
                     {
                         Console.Write("\t");
                     }
+                    // TODO nie wiem, czy tak może być:
+                    // Console.WriteLine(fileName); // cała ścieżka
+                    //var myDirectoryInfo = new DirectoryInfo(path);
 
-                    Console.WriteLine(fileName);
+                    Console.WriteLine(Path.GetFileName(fileName) + "  ");
+                    //Console.Write(ExtensionDirectoryInfo.NumberOfDirectoriesFiles(myDirectoryInfo));
+                    //Console.WriteLine();
                 }
             }
         }
@@ -90,8 +99,18 @@ namespace PTLab1
                     {
                         Console.Write("\t");
                     }
-                    Console.WriteLine(subdirectory);
+                    //var myDirectoryInfo = new DirectoryInfo(path);
+                    var myDirectoryInfo = new DirectoryInfo(subdirectory);
+                    //Console.WriteLine(Path.GetFileName(subdirectory));       // tylko nazwa folderu
+
+                    Console.WriteLine(Path.GetFileName(subdirectory) + "  |||  catalogs: "
+                        + ExtensionDirectoryInfo.NumberOfDirectories(myDirectoryInfo) + "  files: " 
+                        + ExtensionDirectoryInfo.NumberOfFiles(myDirectoryInfo)
+                        );
+
+
                     TreeStructure(subdirectory, countBackslashStartPath);
+
                 }
             }
         }
@@ -105,25 +124,29 @@ namespace PTLab1
             //    Console.WriteLine($"Arg[{i}] = [{args[i]}]");
             //}
 
-            if (args.Length > 0)
+            if (args.Length > 2)
             {
-                Console.WriteLine("ilość argumentów: " + args.Length);
+                //Console.WriteLine("number of arguments: " + args.Length);
                 string startPath = args[0];      // ścieżka pobrana z parametru wywołania programu
                 Console.WriteLine("Start path: " + startPath);
                 //Console.WriteLine("Type of path:" + path.GetType());
 
 
                 // todo tutaj dodać obsługę kolejnych parametrów funkcji
+                string typeOfSort = args[1];
+                Console.WriteLine("typeOfSort: " + typeOfSort);
 
+                string orderOfSort = args[2];
+                Console.WriteLine("orderOfSort: " + orderOfSort + '\n');
 
                 int countBackslashStartPath = startPath.Split('\\').Length - 1;
-                Console.WriteLine("countBackslash = " + countBackslashStartPath);
+                //Console.WriteLine("countBackslash = " + countBackslashStartPath);
 
                 TreeStructure(startPath, countBackslashStartPath);
             }
             else
             {
-                Console.WriteLine("No application arguments");
+                Console.WriteLine("Not enough application arguments");
             }
         }
     }
